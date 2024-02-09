016671e75e686afa45ea1d9fca241b408bf6724f
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-02-09 00:06:46 +0000

    write JS to generate book in html doc

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const myLibrary = [];
 let unreadBooks = myLibrary.filter(book => !book.read);
 let readBooks = myLibrary.filter(book => book.read);
-const slider = document.querySelector('.slide-container');
+const bookContainer = document.querySelector('.book-container');
 
 function Book(title, author, pages, genre, read) {
     this.title = title;
@@ -16,31 +16,31 @@ function getBook() {
     let book = new Book('The Hobbit', 'J.R.R. Tolkein', '310', 'Fantasy', false)
     addToLibrary(book);
 
-    let book1 = new Book('The Hobbit', 'J.R.R. Tolkein', '310', 'Fantasy', false)
+    let book1 = new Book('1The Hobbit', 'J.R.R. Tolkein', '310', 'Fantasy', false)
     addToLibrary(book1);
 
-    let book2 = new Book('The Hobbit', 'J.R.R. Tolkein', '310', 'Fantasy', false)
+    let book2 = new Book('2The Hobbit', 'J.R.R. Tolkein', '310', 'Fantasy', false)
     addToLibrary(book2);
 
-    let book3 = new Book('The Hobbit', 'J.R.R. Tolkein', '310', 'Fantasy', false)
+    let book3 = new Book('3The Hobbit', 'J.R.R. Tolkein', '310', 'Fantasy', false)
     addToLibrary(book3);
 
-    let book4 = new Book('The Hobbit', 'J.R.R. Tolkein', '310', 'Fantasy', false)
+    let book4 = new Book('4The Hobbit', 'J.R.R. Tolkein', '310', 'Fantasy', false)
     addToLibrary(book4);
 
-    let book5 = new Book('The Hobbit', 'J.R.R. Tolkein', '310', 'Fantasy', false)
+    let book5 = new Book('5The Hobbit', 'J.R.R. Tolkein', '310', 'Fantasy', false)
     addToLibrary(book5);
 
-    let book6 = new Book('The Hobbit', 'J.R.R. Tolkein', '310', 'Fantasy', false)
+    let book6 = new Book('6The Hobbit', 'J.R.R. Tolkein', '310', 'Fantasy', false)
     addToLibrary(book6);
 }
 
 function addToLibrary(book) {
-    
+
     myLibrary.push(book);
 
-    const bookContainer = document.createElement('div');
-    bookContainer.classList.add('book-container');
+    const bookCard = document.createElement('div');
+    bookCard.classList.add('book-card');
 
     const title = document.createElement('h3');
     title.textContent = book.title;
@@ -56,42 +56,44 @@ function addToLibrary(book) {
 
     const icon = document.createElement('span');
     icon.classList.add('material-symbols-outlined');
-    icon.textContent = 'import_contacts';
+    icon.textContent = 'swords';
 
-    bookContainer.append(title, dividingLine, author, dividingLine.cloneNode(true), pages, icon);
+    bookCard.append(title, dividingLine, author, dividingLine.cloneNode(true), pages, icon);
 
-    
-    slider.appendChild(bookContainer);
+    bookContainer.appendChild(bookCard);
+
+    leftPosition = -3;
+    topPosition = -2;
+
+    let pageStack = document.createElement('div');
+
+    for (let z = 1; z <= 16; z++) {
+        const bookPage = document.createElement('div');
+        bookPage.style.position = 'absolute';
+        bookPage.style.left = `${leftPosition}px`;
+        bookPage.style.top = `${topPosition}px`;
+        bookPage.style.zIndex = -z;
+        bookPage.classList.add('page');
+        pageStack.appendChild(bookPage);
+        leftPosition += 2;
+        topPosition += 1;
+    }
+
+
+    const backCover = document.createElement('div');
+    backCover.style.position = 'absolute';
+    backCover.classList.add('book-cover');
+    backCover.style.left = `${leftPosition+6}px`;
+    backCover.style.top = `${topPosition+3}px`;
+    backCover.style.zIndex = -17; //must be one less than page count
+    pageStack.appendChild(backCover);
+
+    bookCard.appendChild(pageStack);
+
 };
 
 
-let isDown = false;
-let xStart;
-let scrollLeft;
 
-slider.addEventListener('mousedown', (e) => {
-isDown = true;
-xStart = e.pageX - slider.offsetLeft;
-scrollLeft = slider.scrollLeft;
-console.log(xStart);
-});
-
-slider.addEventListener('mouseleave', () => {
-    isDown = false;
-});
-
-slider.addEventListener('mouseup', () => {
-    isDown = false;
-});
-
-slider.addEventListener('mousemove', (e) => {  
-    if(!isDown) return;
-    e.preventDefault();
-    
-    const x = e.pageX - slider.offsetLeft;
-    const move = (x - xStart) * 1.2;
-    slider.scrollLeft = scrollLeft - move;
-});
 
 
 
